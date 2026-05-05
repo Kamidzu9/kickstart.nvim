09a8a4c8cd6d14e088c6ba0dd87f77e22abf8248
Mykhailo Solovei <senvei228@gmail.com>
2026-05-05 09:34:20 +0200

    added neo-tree, lazy.git

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -10,10 +10,12 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    'kdheepak/lazygit.nvim',
   },
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>lg', '<cmd>LazyGit<CR>', desc = 'LazyGit', silent = true },
   },
   ---@module 'neo-tree'
   ---@type neotree.Config
